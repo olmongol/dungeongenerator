41605d7134d68349307b8a5a08bba97d3682700c
Marcus Schwamberger <marcus@lederzeug.de>
2022-05-22 15:50:58 +0200

    started with dungeon assets: class for doors and entry points

--- src/dungeonmodules/dungeonassets.py
+++ src/dungeonmodules/dungeonassets.py
@@ -12,19 +12,21 @@
 \version 0.1
 '''
 __version__ = "0.1"
-__updated__ = "20.05.2022"
+__updated__ = "22.05.2022"
 __author__ = "Marcus Schwamberger"
 __me__ = "dungeonassets.py"
 
+from copy import deepcopy
 
 
-class access ():
+
+class accesspoint():
     """! objects of this class are access points to rooms, corridors etc. like
     doorways, gateways etc.
     """
 
 
-    def __init__(self, **kargs):
+    def __init__(self, **kwargs):
         """!
         Connstructor
         @param kwargs dictionary of key/value pairs:
@@ -35,6 +37,7 @@ class access ():
             - porculli
             - opening
             - secred door
+            - dimension portal
         - connects room/corridor id(s) it is connecting
         - description
         - state:
@@ -50,4 +53,49 @@ class access ():
         - strength (how easy is it to force entry)
         - silence (how quiet it may be opened/closed)
         """
-        pass
+        params = ["id", "type", "connects", "description", "state", "specials", "lockstrength", "strength", "silence"]
+
+        if "id" in kwargs.keys():
+            self.id = kwargs["id"]
+
+        else:
+            self.id = 0
+
+        if "type" in kwargs.keys():
+            self.type = kwargs["type"]
+        else:
+            self.type = ""
+
+        if "connects" in kwargs.keys():
+            self.connects = kwargs["connects"]
+        else:
+            self.connects = []
+
+        if "description" in kwargs.keys():
+            self.description = kwargs["description"]
+        else:
+            self.description = ""
+
+        if "state" in kwargs.keys():
+            self.state = kwargs["state"]
+        else:
+            self.state = ""
+
+        if "specials" in kwargs.keys():
+            self.specials = kwargs["specials"]
+        else:
+            self.specials = []
+
+        if "lockstrength" in kwargs.keys():
+            self.lockstrength = kwargs["lockstrength"]
+        else:
+            self.lockstrength = 0
+        if "strength" in kwargs.keys():
+            self.strength = kwargs["strength"]
+        else:
+            self.strength = 0
+
+        if "silence" in kwargs.keys():
+            self.silence = kwargs["silence"]
+        else:
+            self.silence = 0
